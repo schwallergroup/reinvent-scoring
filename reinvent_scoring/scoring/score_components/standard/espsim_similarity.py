a463ae5951b8a12be063f4b3366885f53a771798
Jenestin Antonipillai <jenestin.a@hotmail.com>
2023-07-19 00:53:43 +0200

    fix: typo

--- reinvent_scoring/scoring/score_components/standard/espsim_similarity.py
+++ reinvent_scoring/scoring/score_components/standard/espsim_similarity.py
@@ -12,7 +12,7 @@ class EspsimSimilarity(BaseScoreComponent):
         super().__init__(parameters)
         self._similarity = Similarity()
         smiles = self.parameters.specific_parameters.get(self.component_specific_parameters.SMILES, [])
-        self._ref_mols = self._similarity.smiles_to_mols(smiles)
+        self._ref_mols = self._similarity.smiles_to_mol(smiles)
 
     def calculate_score(self, molecules: List) -> ComponentSummary:
         query_smis = [Chem.MolToSmiles(mol) for mol in molecules]
